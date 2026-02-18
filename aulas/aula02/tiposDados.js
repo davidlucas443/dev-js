849b105a2b265f76973d79488a9dcaa85e34b95a
davidlucas443 <davidga19391@gmail.com>
2026-02-17 22:02:35 -0300

    feat: created aula02

---
+++ aulas/aula02/tiposDados.js
@@ -0,0 +1,24 @@
+let nome = "Dois";
+let idade = 17
+let aprovado = 1;
+let reprovado = 0;
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+console.log(typeof aprovado);
+console.log(typeof reprovado);
+
+nome = 2;
+idade = "dezessete";
+aprovado = true;
+reprovado = false;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof aprovado);
+console.log(typeof reprovado);
+
+// Converti `nome` de string para número removendo as aspas
+// Converti `idade` de número para string envolvendo o valor com aspas
+// Converti `aprovado` e `reprovado` para booleanos usando `true` e `false`

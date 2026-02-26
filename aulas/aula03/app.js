d26359f259367aa9b63edea64b20902b562dfc6e
DAVID LUCAS DE OLIVEIRA SOUSA <david.sousa12@senaisp.edu.br>
2026-02-26 08:16:07 -0300

    feat: created aula03

---
+++ aulas/aula03/app.js
@@ -0,0 +1,16 @@
+let nome = "David";
+let idade = 17;
+let cidade = "São Paulo";
+
+let num1 = 17;
+let num2 = 17;
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
+
+
+console.log("Soma: " + (num1 + num2));
+console.log("Subtração: " + (num1 - num2));
+console.log("Multiplicação: " + (num1 * num2));
+console.log("Divisão: " + (num1 / num2));
+
+console.log(`Meu nome é ${nome} e moro em ${cidade}.`);
